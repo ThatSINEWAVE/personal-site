8f6f7ce27592e57ce5dcefd17f7310aae946a5ed
David <133239148+ThatSINEWAVE@users.noreply.github.com>
2024-04-28 05:47:33 +0300

    Update header.tsx

--- app/blog/[slug]/header.tsx
+++ app/blog/[slug]/header.tsx
@@ -88,7 +88,7 @@ export const Header: React.FC<Props> = ({ project, views }) => {
 					</div>
 
 					<Link
-						href="/projects"
+						href="/blog"
 						className={`duration-200 hover:font-medium ${
 							isIntersecting
 								? " text-zinc-400 hover:text-zinc-100"
