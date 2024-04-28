e0ced36a41c55a0aa505e7423ed014a2f256500b
David <133239148+ThatSINEWAVE@users.noreply.github.com>
2024-04-28 05:44:01 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Particles from "./components/particles";
 
 const navigation = [
   { name: "Projects", href: "/projects" },
+  { name: "blog", href: "/blog" },
   { name: "Contact", href: "/contact" },
 ];
 
@@ -35,7 +36,7 @@ export default function Home() {
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <div className="my-16 text-center animate-fade-in">
         <h2 className="text-sm text-zinc-500 ">
-          I'm building to solve API authentication and authorization for developers.
+          Security Researcher and Entrepreneur. Let's innovate!
         </h2>
       </div>
     </div>
