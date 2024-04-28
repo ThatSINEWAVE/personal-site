98879ccf72d0e767dce0848dc9ac465f9b8fdfb6
David <133239148+ThatSINEWAVE@users.noreply.github.com>
2024-04-28 05:56:26 +0300

    Update page.tsx

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -20,6 +20,17 @@ export default async function ProjectsPage() {
     return acc;
   }, {} as Record<string, number>);
 
+  const featured = allProjects.find((project) => project.slug === "unkey")!;
+  const top2 = allProjects.find((project) => project.slug === "planetfall")!;
+  const top3 = allProjects.find((project) => project.slug === "highstorm")!;
+  const sorted = allProjects
+    .filter((p) => p.published)
+    .filter(
+      (project) =>
+        project.slug !== featured.slug &&
+        project.slug !== top2.slug &&
+        project.slug !== top3.slug,
+    )
     .sort(
       (a, b) =>
         new Date(b.date ?? Number.POSITIVE_INFINITY).getTime() -
