236452cd67ae2b5ecab1900cf7c98ad18789e9e0
David <133239148+ThatSINEWAVE@users.noreply.github.com>
2024-04-29 05:41:51 +0300

    Update about.tsx

--- app/about/about.tsx
+++ app/about/about.tsx
@@ -2,7 +2,7 @@
 import { Navigation } from "../components/nav";
 import { Card } from "../components/card";
 
-export default function AboutPage() {
+export default function Example() {
   return (
     <div className="bg-gradient-to-tl from-zinc-900/0 via-zinc-900 to-zinc-900/0">
       <Navigation />
