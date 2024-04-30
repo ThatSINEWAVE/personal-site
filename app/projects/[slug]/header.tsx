86523a117387c81cddb5fcd6adf7d1174453fa8c
David <133239148+ThatSINEWAVE@users.noreply.github.com>
2024-04-30 07:09:42 +0300

    Update header.tsx

--- app/projects/[slug]/header.tsx
+++ app/projects/[slug]/header.tsx
@@ -67,7 +67,7 @@ export const Header: React.FC<Props> = ({ project, views }) => {
 								views,
 							)}
 						</span>
-						<Link target="_blank" href="https://twitter.com/ThatSINEWAVE">
+						<Link target="_blank" href="https://twitter.com/intent/user?screen_name=ThatSINEWAVE">
 							<Twitter
 								className={`w-6 h-6 duration-200 hover:font-medium ${
 									isIntersecting
